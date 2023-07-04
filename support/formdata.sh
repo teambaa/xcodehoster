89bcbd76908868b70e82b7eff9faa5e1b21f19af
teambaa <122075564+teambaa@users.noreply.github.com>
2023-07-04 11:58:44 +0700

    Update formdata.sh

--- support/formdata.sh
+++ support/formdata.sh
@@ -18,7 +18,7 @@ cat <<EOT
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Pendaftaran Xcodehoster</title>
+    <title>Pendaftaran Hosting Gratis</title>
      <link rel="stylesheet" href="https://xcodehoster.com/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <style>
     { 
